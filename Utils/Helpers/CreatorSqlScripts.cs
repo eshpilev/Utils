1ac9a002dfa03aa309a88bbe9b73d7701b8522ee
eshpilev <e.a.shpilev@mail.ru>
2024-02-04 15:58:48 +0300

    Add PeriodBatchesEnumerator, CreatorSqlScripts

---
+++ Utils/Helpers/CreatorSqlScripts.cs
@@ -0,0 +1,130 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using System.Globalization;
+using System.Text;
+
+namespace Utils.Helpers
+{
+    /// <summary> Генерация SQL скрипта создания таблиц из csv файла </summary>
+    public class CreatorSqlScripts
+    {
+        private const string Dir = "c:\\Work";
+        private const string SchemaName = "fedres";
+        private const string DescCsvFile = $"{Dir}\\Descriptions.csv";
+        private const string CreatorFile = $"{Dir}\\Creator.txt";
+        private const string RollbackFile = $"{Dir}\\Rollback.txt";
+        private const string TableDetector = "#";
+        private const string Marked = "1";
+        private Stack<string> _tables;
+        private StringBuilder _fields;
+        private StringBuilder _constaintPk;
+        private StringBuilder _constaintFk;
+
+        public static void Run()
+        {
+            new CreatorSqlScripts().Process();
+        }
+
+        public void Process()
+        {
+            using var writer = new StreamWriter(CreatorFile);
+            _tables = new Stack<string>();
+            _fields = new StringBuilder();
+            _constaintPk = new StringBuilder();
+            _constaintFk = new StringBuilder();
+            var descriptions = ReadCsv();
+            var table = string.Empty;
+            foreach (var desc in descriptions)
+            {
+                if (desc.Name.StartsWith(TableDetector))
+                {
+                    if (_tables.Count > 0)
+                        AddEndTable(writer);
+                    table = desc.Name.Replace(TableDetector, "");
+                    AddStartTable(writer, table);
+                    _tables.Push(table);
+                }
+                else
+                {
+                    if (_fields.Length > 0) _fields.Append(",");
+                    _fields.Append($"\n\t{desc.Name} {SqlType(desc.Type)} {SqlNull(desc.Mandatory)}");
+                    if (!string.IsNullOrEmpty(desc.Identity)) _fields.Append($" {SqlIdentity(desc.Identity)}");
+                    if (desc.PK == Marked)
+                    {
+                        if (_constaintPk.Length > 0)
+                        {
+                            _constaintPk.Replace(")", $", {desc.Name})");
+                        }
+                        else
+                            _constaintPk.Append($"\n\tCONSTRAINT {table}_pkey PRIMARY KEY ({desc.Name})");
+                    }
+                    if (!string.IsNullOrEmpty(desc.FK))
+                    {
+                        if (_constaintFk.Length > 0) _constaintFk.Append(",");
+                        _constaintFk.Append($"\n\tCONSTRAINT fk_{table}_{desc.Name} FOREIGN KEY ({desc.Name})");
+                        _constaintFk.Append($"\n\t\tREFERENCES {SchemaName}.{desc.FK}");
+                        if (!string.IsNullOrEmpty(desc.DeleteAction))
+                            _constaintFk.Append($"\n\t\tON UPDATE {desc.DeleteAction} ON DELETE {desc.DeleteAction}");
+                    }
+                }
+            }
+            if (_tables.Count > 0)
+            {
+                AddEndTable(writer);
+                using var writerRollback = new StreamWriter(RollbackFile);
+                foreach (var tableName in _tables)
+                {
+                    writerRollback.WriteLine($"DROP TABLE {SchemaName}.{tableName} CASCADE;");
+                }
+            }
+        }
+        private void AddStartTable(TextWriter writer, string table)
+        {
+            _fields.Clear();
+            _constaintPk.Clear();
+            _constaintFk.Clear();
+            writer.Write($"CREATE TABLE {SchemaName}.{table}(");
+        }
+        private void AddEndTable(TextWriter writer)
+        {
+            writer.Write(_fields);
+            if (_constaintPk.Length > 0)
+                writer.Write($",{_constaintPk}");
+            if (_constaintFk.Length > 0)
+                writer.Write($",{_constaintFk}");
+            writer.WriteLine("\n);\n");
+        }
+        private IEnumerable<DescriptionFields> ReadCsv()
+        {
+            using var reader = new StreamReader(DescCsvFile);
+            using var csvReader = new CsvReader(reader, new CsvConfiguration(new CultureInfo("ru-RU"))
+            {
+                Delimiter = ";",
+                HasHeaderRecord = true
+            });
+            return csvReader.GetRecords<DescriptionFields>().ToList();
+        }
+        private static string SqlType(string value) =>
+            value.Replace("string", "varchar").Replace("date", "timestamp");
+        private static string SqlNull(string value) => value == Marked ? "NOT NULL" : "NULL";
+        private static string SqlIdentity(string value) => value == Marked ? "GENERATED ALWAYS AS IDENTITY" : "";
+    }
+    public class DescriptionFields
+    {
+        [Name("Name")]
+        public string Name { get; set; }
+        [Name("Type")]
+        public string Type { get; set; }
+        [Name("Mandatory")]
+        public string Mandatory { get; set; }
+        [Name("Identity")]
+        public string Identity { get; set; }
+        [Name("PK")]
+        public string PK { get; set; }
+        [Name("FK")]
+        public string FK { get; set; }
+        [Name("Delete Action")]
+        public string DeleteAction { get; set; }
+    }
+}
---
+++ Utils/Helpers/PeriodBatchesEnumerator.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+
+namespace Utils.Helpers
+{
+    /// <summary> Итератор возвращающий диапазон строк с разбивкой по периоду </summary>
+    public class PeriodBatchesEnumerator<T> : IEnumerator<IEnumerable<T>> where T : IRecord
+    {
+        public PeriodBatchesSettings Settings { get; set; }
+        public IOrderedQueryable<T> Query { get; set; }
+        private DateTime _fromDate;
+        private DateTime _toDate;
+        private bool _getNextPeriod;
+        private int _batch;
+        public PeriodBatchesEnumerator(PeriodBatchesSettings settings)
+        {
+            ValidateSettings(settings);
+            Settings = settings;
+            _fromDate = InitFromDate();
+            _toDate = InitToDate();
+        }
+        private void ValidateSettings(PeriodBatchesSettings settings)
+        {
+            if (settings.BatchSize <= 0)
+                throw new ArgumentException("Не задан размер одной пачки");
+            if (settings.FromDate == DateTime.MinValue || settings.FromDate == DateTime.MaxValue)
+                throw new ArgumentException("Не задана минимальная дата");
+            if (settings.ToDate == DateTime.MinValue || settings.ToDate == DateTime.MaxValue)
+                throw new ArgumentException("Не задана максимальная дата");
+            if (settings.FromDate > settings.ToDate)
+                throw new ArgumentException("Минимальная дата больше максимальной даты");
+            if (Query != null)
+                throw new ArgumentNullException(nameof(Query));
+        }
+        public IEnumerable<T> Current
+        {
+            get
+            {
+                var messages = Query
+                    .Where(x => x.CreateDateTime >= _fromDate && x.CreateDateTime < _toDate)
+                    .Skip(_batch * Settings.BatchSize)
+                    .Take(Settings.BatchSize)
+                    .ToArray();
+                _getNextPeriod = messages.Length < Settings.BatchSize || messages.Length == 0;
+                _batch++;
+                return messages;
+            }
+        }
+        public bool MoveNext()
+        {
+            if (_getNextPeriod)
+            {
+                _batch = 0;
+                _fromDate = _toDate;
+                _toDate = InitToDate();
+            }
+            return _fromDate <= Settings.ToDate;
+        }
+        private DateTime InitFromDate() =>
+            Settings.Periodicity switch
+            {
+                Periodicity.Daily => Settings.FromDate.Date,
+                Periodicity.Monthly => new DateTime(Settings.FromDate.Year, Settings.FromDate.Month, 1),
+                Periodicity.Yearly => new DateTime(Settings.FromDate.Year, 1, 1),
+                _ => throw new ArgumentOutOfRangeException(nameof(Settings.Periodicity), Settings.Periodicity, null)
+            };
+
+        private DateTime InitToDate() =>
+            Settings.Periodicity switch
+            {
+                Periodicity.Daily => _fromDate.AddDays(1),
+                Periodicity.Monthly => _fromDate.AddMonths(1),
+                Periodicity.Yearly => _fromDate.AddYears(1),
+                _ => throw new ArgumentOutOfRangeException(nameof(Settings.Periodicity), Settings.Periodicity, null)
+            };
+
+        public void Reset()
+        {
+            _fromDate = InitFromDate();
+            _toDate = InitToDate();
+            _batch = 0;
+        }
+        object IEnumerator.Current => Current;
+        public void Dispose() { }
+    }
+
+    public class PeriodBatchesEnumeratorDemo : PeriodBatchesEnumerator<DemoRecord>
+    {
+        public PeriodBatchesEnumeratorDemo(PeriodBatchesSettings settings) : base(settings)
+        {
+        }
+        public static void Run()
+        {
+            var fromDate = new DateTime(2024, 01, 01);
+            var toDate = new DateTime(2024, 12, 31);
+            using var batches = new PeriodBatchesEnumerator<DemoRecord>(new PeriodBatchesSettings
+            {
+                BatchSize = 10,
+                Periodicity = Periodicity.Monthly,
+                FromDate = fromDate,
+                ToDate = toDate
+            });
+            var records = new List<DemoRecord> { };
+            var i = 0;
+            for (var currentDate = fromDate; currentDate < toDate; currentDate = currentDate.AddDays(1))
+            {
+                records.Add(new DemoRecord
+                {
+                    Hash = $"{i++}",
+                    CreateDateTime = currentDate
+                });
+            }
+            batches.Query = records.AsQueryable().OrderBy(x => x.Hash);
+            var batchNum = 0;
+            while (batches.MoveNext())
+            {
+                Console.WriteLine($"BatchNum:{batchNum}");
+                foreach (var record in batches.Current!)
+                    Console.WriteLine($"Hash:{record.Hash}, CreateDateTime:{record.CreateDateTime}");
+                batchNum++;
+            }
+        }
+    }
+
+    public class PeriodBatchesSettings
+    {
+        public int BatchSize { get; set; }
+        public Periodicity Periodicity { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+    }
+
+    public interface IRecord
+    {
+        public DateTime CreateDateTime { get; set; }
+    }
+    public enum Periodicity
+    {
+        Daily,
+        Monthly,
+        Yearly
+    }
+    public struct DemoRecord : IRecord
+    {
+        public string Hash { get; set; }
+        public DateTime CreateDateTime { get; set; }
+    }
+}
